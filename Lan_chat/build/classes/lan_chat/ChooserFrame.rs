37c0e06e912964001705c92d68437109195694e6
duyuit <duykkxm92@gmail.com>
2018-06-14 11:51:26 +0700

    last Commit

--- Lan_chat/build/classes/lan_chat/ChooserFrame.rs
+++
@@ -1,8 +0,0 @@
-lantrans.ChooserFrame$2
-lantrans.ChooserFrame$1
-lantrans.ChooserFrame$GBC
-lantrans.ChooserFrame
-lantrans.ChooserFrame$6
-lantrans.ChooserFrame$5
-lantrans.ChooserFrame$4
-lantrans.ChooserFrame$3
